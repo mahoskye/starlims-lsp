1710e6bed665be9c2947c5b4c36f4e026fa9f3cf
Benjamin Buddle <mahoskye@gmail.com>
2026-08-26 23:32:26 -0600

    style: gofmt internal/server/server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -41,8 +41,8 @@ type SignatureHelpSettings struct {
 
 // DiagnosticsSettings represents diagnostics settings from the client.
 type DiagnosticsSettings struct {
-	HungarianNotation *bool              `json:"hungarianNotation"`
-	UnusedVariables   *bool              `json:"unusedVariables"`
+	HungarianNotation *bool `json:"hungarianNotation"`
+	UnusedVariables   *bool `json:"unusedVariables"`
 	// UnicodeLiteralPrefix / CollateJustification toggle the opt-in
 	// embedded-SQL style rules unicode_literal_prefix (issue #196) and
 	// unjustified_collate (issue #197). Both default off.
@@ -51,12 +51,12 @@ type DiagnosticsSettings struct {
 	// SpacedSkipCommas / VisibilityAnnotationUsage toggle the opt-in
 	// style rules spaced_skip_commas (issue #193) and
 	// visibility_annotation_usage (issue #198). Both default off.
-	SpacedSkipCommas          *bool `json:"spacedSkipCommas"`
-	VisibilityAnnotationUsage *bool `json:"visibilityAnnotationUsage"`
-	HungarianPrefixes *[]string          `json:"hungarianPrefixes"`
-	Globals           *[]string          `json:"globals"`
-	MaxBlockDepth     *int               `json:"maxBlockDepth"`
-	Rules             *map[string]string `json:"rules"`
+	SpacedSkipCommas          *bool              `json:"spacedSkipCommas"`
+	VisibilityAnnotationUsage *bool              `json:"visibilityAnnotationUsage"`
+	HungarianPrefixes         *[]string          `json:"hungarianPrefixes"`
+	Globals                   *[]string          `json:"globals"`
+	MaxBlockDepth             *int               `json:"maxBlockDepth"`
+	Rules                     *map[string]string `json:"rules"`
 	// EndpointPatterns is a list of substrings/suffixes; any URI whose
 	// lowercased path contains one of these is treated as an endpoint
 	// script (Request/Response are then pre-injected ambients). In
